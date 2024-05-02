a335bb828111cba4db0a72ffe2e3294dd8259347
AmadoJunior <amadoelrepalol@gmail.com>
2024-05-02 18:02:21 -0400

    Removed ripple effect from loading component

--- web-components/loading-button.js
+++ web-components/loading-button.js
@@ -77,21 +77,6 @@ class LoadingButton extends HTMLElement {
 						transform: rotate(360deg);
 					}
 				}
-
-				.ripple {
-					position: absolute;
-					border-radius: 50%;
-					transform: scale(0);
-					animation: ripple 600ms linear;
-					background-color: rgba(255, 255, 255, 0.7);
-				}
-
-				@keyframes ripple {
-					to {
-						transform: scale(4);
-						opacity: 0;
-					}
-				}
 			</style>
 
 			<button>
@@ -107,7 +92,6 @@ class LoadingButton extends HTMLElement {
 
   	handleClick (event) {
 		if (!this.props.loading && this.onClick && typeof this.onClick === 'function') {
-			this.createRipple(event);
 			this.onClick();
 		}
 	}
@@ -129,26 +113,6 @@ class LoadingButton extends HTMLElement {
 			this.props[name] = newValue
 			this.render();
 		}
-	}
-
-  	// Ripple Effect
-	createRipple(event) {
-		const button = event.currentTarget;
-		const circle = document.createElement("span");
-		const diameter = Math.max(button.clientWidth, button.clientHeight);
-		const radius = diameter / 2;
-		circle.style.width = circle.style.height = `${diameter}px`;
-		circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
-		circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
-		circle.classList.add("ripple"); 
-
-		const ripple = button.getElementsByClassName("ripple")[0];
-
-		if (ripple) {
-			ripple.remove();
-		}
-
-		button.appendChild(circle);
 	}
 
 	// Render Method
